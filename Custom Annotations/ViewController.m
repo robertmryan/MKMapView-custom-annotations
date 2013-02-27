1c500704fc1c41f3535ebd2bb71e7c472b579857
Robert Ryan <rryan@Robert-Ryans-iMac.local>
2013-02-27 18:40:43 -0500

    1. Use serial `NSOperationQueue` to ensure we don't accidentally trigger concurrent searches; 2. No longer remove old annotations, and instead check to MKPlacemark resulting from MKLocalSearch needs to be added, or if we already have it; and 3. Animate the dropping of pins on the map.

--- Custom Annotations/ViewController.m
+++ Custom Annotations/ViewController.m
@@ -16,6 +16,7 @@
 @interface ViewController () <MKMapViewDelegate, ABUnknownPersonViewControllerDelegate>
 
 @property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, strong) NSOperationQueue *searchQueue;
 
 @end
 
@@ -25,11 +26,13 @@
 {
     [super viewDidLoad];
 
+    self.searchQueue = [[NSOperationQueue alloc] init];
+    self.searchQueue.maxConcurrentOperationCount = 1;
+    
     [self.mapView setUserTrackingMode:MKUserTrackingModeFollow];
     
 	// Do any additional setup after loading the view, typically from a nib.
 }
-
 
 #pragma mark - MKMapViewDelegate
 
@@ -41,30 +44,61 @@
     
     CGFloat meters = MKMetersBetweenMapPoints(eastMapPoint, westMapPoint);
 
-    if ([mapView.annotations count] > 0)
-        [mapView removeAnnotations:mapView.annotations];
-
     if (meters > 5000)
         return;
     
-    MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
-    request.naturalLanguageQuery = @"restaurant";
-    request.region = mapView.region;
+    // if we have some backlogged requests, let's just get rid of them
     
-    MKLocalSearch *localSearch = [[MKLocalSearch alloc] initWithRequest:request];
-    [localSearch startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+    [self.searchQueue cancelAllOperations];
+    
+    // issue new MKLoadSearch
+    
+    [self.searchQueue addOperationWithBlock:^{
 
-        NSMutableArray *annotations = [NSMutableArray array];
+        __block BOOL done = NO;
         
-        [response.mapItems enumerateObjectsUsingBlock:^(MKMapItem *item, NSUInteger idx, BOOL *stop) {
-            CustomAnnotation *annotation = [[CustomAnnotation alloc] initWithPlacemark:item.placemark];
-            annotation.title = item.name;
-            annotation.phone = item.phoneNumber;
-            annotation.subtitle = item.placemark.addressDictionary[(NSString *)kABPersonAddressStreetKey];
-            [annotations addObject:annotation];
+        MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
+        request.naturalLanguageQuery = @"restaurant";
+        request.region = mapView.region;
+        
+        MKLocalSearch *localSearch = [[MKLocalSearch alloc] initWithRequest:request];
+        [localSearch startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error) {
+            
+            NSMutableArray *annotations = [NSMutableArray array];
+            
+            [response.mapItems enumerateObjectsUsingBlock:^(MKMapItem *item, NSUInteger idx, BOOL *stop) {
+                
+                for (CustomAnnotation *annotation in mapView.annotations)
+                {
+                    // if we don't need this one, don't add it, just return, and we'll check the next one
+                    
+                    if ([annotation isKindOfClass:[CustomAnnotation class]])
+                        if (item.placemark.coordinate.latitude == annotation.coordinate.latitude &&
+                            item.placemark.coordinate.longitude == annotation.coordinate.longitude)
+                        {
+                            return;
+                        }
+                }
+                
+                // otherwise add it
+                
+                CustomAnnotation *annotation = [[CustomAnnotation alloc] initWithPlacemark:item.placemark];
+                annotation.title = item.name;
+                annotation.phone = item.phoneNumber;
+                annotation.subtitle = item.placemark.addressDictionary[(NSString *)kABPersonAddressStreetKey];
+                [annotations addObject:annotation];
+            }];
+
+            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                [self.mapView addAnnotations:annotations];
+            }];
+            
+            done = YES;
         }];
         
-        [self.mapView addAnnotations:annotations];
+        while (!done)
+            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode
+                                     beforeDate:[NSDate distantFuture]];
     }];
 }
 
@@ -111,6 +145,59 @@
     
     CFRelease(address);
     CFRelease(person);
+}
+
+// Animation from http://stackoverflow.com/a/7045872/1271826
+
+- (void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray *)views
+{
+    MKAnnotationView *annotationView;
+    
+    for (annotationView in views) {
+        
+        // Don't pin drop if annotation is user location
+        if ([annotationView.annotation isKindOfClass:[MKUserLocation class]]) {
+            continue;
+        }
+        
+        // Check if current annotation is inside visible map rect, else go to next one
+        MKMapPoint point =  MKMapPointForCoordinate(annotationView.annotation.coordinate);
+        if (!MKMapRectContainsPoint(self.mapView.visibleMapRect, point)) {
+            continue;
+        }
+        
+        CGRect endFrame = annotationView.frame;
+        
+        // Move annotation out of view
+        annotationView.frame = CGRectMake(annotationView.frame.origin.x, annotationView.frame.origin.y - self.view.frame.size.height, annotationView.frame.size.width, annotationView.frame.size.height);
+        
+        // Animate drop
+        [UIView animateWithDuration:0.4
+                              delay:0.04*[views indexOfObject:annotationView]
+                            options:UIViewAnimationOptionCurveLinear
+                         animations:^{
+                             
+                             annotationView.frame = endFrame;
+                             
+                             // Animate squash
+                         }
+                         completion:^(BOOL finished){
+                             if (finished) {
+                                 [UIView animateWithDuration:0.05
+                                                  animations:^{
+                                                      annotationView.transform = CGAffineTransformMakeScale(1.0, 0.8);
+                                                      
+                                                  }
+                                                  completion:^(BOOL finished){
+                                                      if (finished) {
+                                                          [UIView animateWithDuration:0.1 animations:^{
+                                                              annotationView.transform = CGAffineTransformIdentity;
+                                                          }];
+                                                      }
+                                                  }];
+                             }
+                         }];
+    }
 }
 
 - (void)unknownPersonViewController:(ABUnknownPersonViewController *)unknownPersonView didResolveToPerson:(ABRecordRef)person
